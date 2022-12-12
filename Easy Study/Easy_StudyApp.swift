c4ccdcd5795a7aa122747106950ee4def8d8a7b9
Vladimir Caterov <vladcaterovdev@gmail.com>
2022-12-12 13:54:06 -0600

    Initial Commit

---
+++ Easy Study/Easy_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_StudyApp.swift
+//  Easy Study
+//
+//  Created by Vladimir Caterov on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Easy_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
